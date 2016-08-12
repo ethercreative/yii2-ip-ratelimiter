1d66d10413c2e5606adc97b21606ce8f3d315821
Yaroslav Lukyanov <c_sharp@mail.ru>
2016-08-12 11:43:18 +0700

    Added interface for IpRateLimit Some changes according to new interface Version up

--- RateLimiter.php
+++ RateLimiter.php
@@ -3,54 +3,59 @@
 namespace ethercreative\ratelimiter;
 
 use Yii;
-use ethercreative\ratelimiter\User;
 
+/**
+ * Class RateLimiter
+ *
+ * @package ethercreative\ratelimiter
+ */
 class RateLimiter extends \yii\filters\RateLimiter
 {
-	/**
-	 * @var boolean whether to separate rate limiting between non and authenticated users
-	 */
-	public $separateRates = true;
+    /**
+     * @var boolean whether to separate rate limiting between non and authenticated users
+     */
+    public $separateRates = true;
 
-	/**
-	 * @var integer the maximum number of allowed requests
-	 */
-	public $rateLimit;
+    /**
+     * @var integer the maximum number of allowed requests
+     */
+    public $rateLimit;
 
-	/**
-	 * @var integer the time period for the rates to apply to
-	 */
-	public $timePeriod;
+    /**
+     * @var integer the time period for the rates to apply to
+     */
+    public $timePeriod;
 
-	/**
-	 * @inheritdoc
-	 */
-	public function beforeAction($action)
-	{
-		$user = $this->user;
+    /**
+     * @inheritdoc
+     */
+    public function beforeAction($action)
+    {
+        $user = $this->user;
 
-		if ($this->separateRates)
-			$user = $user ?: (Yii::$app->getUser() ? Yii::$app->getUser()->getIdentity(false) : null);
+        if ($this->separateRates) {
+            $user = $user ?: (Yii::$app->getUser() ? Yii::$app->getUser()->getIdentity(false) : null);
+        }
 
-		$user = $user ?: User::findByIp(Yii::$app->request->userIP, $this->rateLimit, $this->timePeriod);
+        /** @var IpRateLimitInterface $identityClass */
+        $identityClass = Yii::$app->getUser()->identityClass;
 
-		if ($user instanceof RateLimitInterface)
-		{
-			return parent::beforeAction($action);
-		}
-		else
-		{
-			Yii::trace('Check rate limit', __METHOD__);
-			$this->checkRateLimit(
-				$user,
-				$this->request ? : Yii::$app->getRequest(),
-				$this->response ? : Yii::$app->getResponse(),
-				$action
-			);
+        $user = $user ?: $identityClass::findByIp(Yii::$app->getRequest()->getUserIP(), $this->rateLimit,
+            $this->timePeriod);
 
-			return true;
-		}
+        if ($user instanceof IpRateLimitInterface) {
+            Yii::trace('Check rate limit', __METHOD__);
 
-		return parent::beforeAction($action);
-	}
+            $this->checkRateLimit(
+                $user,
+                $this->request ?: Yii::$app->getRequest(),
+                $this->response ?: Yii::$app->getResponse(),
+                $action
+            );
+
+            return true;
+        }
+
+        return parent::beforeAction($action);
+    }
 }
---
+++ UserExample.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace ethercreative\ratelimiter;
+
+use Yii;
+
+/**
+ * Class UserExample
+ *
+ * @package ethercreative\ratelimiter
+ */
+class UserExample extends \yii\base\Model implements IpRateLimitInterface
+{
+    /**
+     * @var string IP of the user
+     */
+    private $ip;
+
+    /**
+     * @var integer maximum number of allowed requests
+     */
+    private $rateLimit;
+
+    /**
+     * @var integer time period for the rates to apply to
+     */
+    private $timePeriod;
+
+    /**
+     * @inheritdoc
+     */
+    public static function findByIp($ip, $rateLimit, $timePeriod)
+    {
+        $user = new static();
+
+        $user->ip = $ip;
+        $user->rateLimit = $rateLimit;
+        $user->timePeriod = $timePeriod;
+
+        return $user;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getRateLimit($request, $action)
+    {
+        return [$this->rateLimit, $this->timePeriod];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function loadAllowance($request, $action)
+    {
+        $cache = Yii::$app->getCache();
+
+        return [
+            $cache->get('user.ratelimit.ip.allowance.' . $this->ip),
+            $cache->get('user.ratelimit.ip.allowance_updated_at.' . $this->ip),
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function saveAllowance($request, $action, $allowance, $timestamp)
+    {
+        $cache = Yii::$app->getCache();
+
+        $cache->set('user.ratelimit.ip.allowance.' . $this->ip, $allowance);
+        $cache->set('user.ratelimit.ip.allowance_updated_at.' . $this->ip, $timestamp);
+    }
+}
